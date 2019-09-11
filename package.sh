77d5070a13973ff02a853b7d4b45631e868714bb
scorpiosixnine <scorpiosixnine@protonmail.com>
2019-09-11 15:16:44 +0100

    updated 1.0.0a2

--- package.sh
+++ package.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
 VERSION="1.0.0a2"
-FOLDER="Releases/Smalls-$VERSION"
-mkdir -p "$FOLDER/Source"
-cp Smalls.esp "$FOLDER/"
-cp -r Source/Scripts "$FOLDER/Source/Scripts"
-cp -r Scripts "$FOLDER/Scripts"
+NAME="Smalls-$VERSION"
+FOLDER="Releases/Smalls"
+ARCHIVE="$NAME.zip"
+
+DATA="/mnt/d/Steam/steamapps/common/Skyrim Special Edition/Data"
+cp "$DATA/Smalls.esp" .
+
+
+mkdir -p "$FOLDER/Smalls/Source"
+cp Smalls.esp "$FOLDER/Smalls/"
+cp -r Source/Scripts "$FOLDER/Smalls/Source/Scripts"
+cp -r Scripts "$FOLDER/Smalls/Scripts"
 cp -r fomod "$FOLDER/"
+
+rm "Releases/$ARCHIVE"
+cd "Releases/Smalls"
+zip -r "../$ARCHIVE" "fomod" "Smalls"
+cd ../..
+rm -rf "$FOLDER"
