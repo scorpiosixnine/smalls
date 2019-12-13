c5fa6c5189fe7e8b3eeb743b190763eea820dc3d
scorpiosixnine <scorpiosixnine@protonmail.com>
2019-12-13 15:42:04 +0000

    fixed version number

--- module.sh
+++ module.sh
@@ -2,7 +2,7 @@
 export MODULE_NAME=Smalls
 export MAJOR=1
 export MINOR=1
-export PATCH=0
+export PATCH=1
 export GIT_NO=`git log --oneline | wc -l`
 export BUILD_NO=$(($GIT_NO + 20))
 export VERSION="$MAJOR.$MINOR.$PATCH"
