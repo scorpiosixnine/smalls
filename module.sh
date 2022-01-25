5d98d72b922db931a198ee023eb4b50045736515
scorpiosixnine <scorpiosixnine@protonmail.com>
2022-01-25 13:15:35 +0000

    added jcontainers sdk

--- module.sh
+++ module.sh
@@ -6,7 +6,7 @@ export PATCH=5
 export GIT_NO=`git log --oneline | wc -l`
 export BUILD_NO=$(($GIT_NO + 20))
 export VERSION="$MAJOR.$MINOR.$PATCH"
-export SDKS="sdks/skyui/dist/Data/Scripts/Headers;sdks/skse64-sdk;sdks/skyrim-sdk/Source/Scripts"
+export SDKS="sdks/skyui/dist/Data/Scripts/Headers;sdks/skse64-sdk;sdks/jcontainers-sdk;sdks/skyrim-sdk/Source/Scripts"
 
 echo "Module: $MODULE_NAME"
 echo "Version is $VERSION ($BUILD_NO)"
